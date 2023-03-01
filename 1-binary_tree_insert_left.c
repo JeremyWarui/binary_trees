16cd6efc52ddb64de5b93be210ab220699acce86
JeremyWarui <jerywarne7@gmail.com>
2023-03-01 22:41:35 +0300

    Task 1: left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	tmp->parent = parent;
 	tmp->n = value;
+	tmp->left = parent->left;
+	tmp->right = NULL;
 
 	if (parent->left != NULL)
 		parent->left->parent = tmp;

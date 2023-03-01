0b7b9cd8bae1fe3ee8b5784fdef9113a4cc65d74
JeremyWarui <jerywarne7@gmail.com>
2023-03-01 22:34:53 +0300

    Add helper functions on header file

--- binary_trees.h
+++ binary_trees.h
@@ -24,4 +24,13 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s avl_t;
 
+/* creates binary tree */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/* insert node on left side */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/* insert node on right side */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/* check if node is root */
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif
